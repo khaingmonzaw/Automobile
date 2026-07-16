b4fddbf612e1b1020e334e99b0a5cf7a57175916
khaingmonzaw <150674210+khaingmonzaw@users.noreply.github.com>
2026-07-16 19:50:02 +0630

    Add user list search and update status badge style

--- mybackend/Admin/UserList.js
+++ mybackend/Admin/UserList.js
@@ -4,6 +4,22 @@ const bcrypt = require("bcrypt");
 
 exports.userLists = (db) => {
   return async (req, res) => {
+     const updatePolicyStatus = `
+      UPDATE policies
+      SET status = 'inactive'
+      WHERE CURDATE() > DATE_ADD(end_date, INTERVAL 3 DAY)
+      AND status = 'active'
+    `;
+
+
+    db.query(updatePolicyStatus, (updateErr,result) => {
+
+      if (updateErr) {
+        console.log(updateErr);
+        return res.status(500).json(updateErr);
+      }
+      console.log("Updated Rows:", result.affectedRows);
+
     const sql = `
     SELECT 
         u.id AS User_ID, 
@@ -11,7 +27,7 @@ exports.userLists = (db) => {
         p.policy_number AS Policy_Number, 
         p.policy_id AS Policy_ID,
        COUNT(c.claim_id) AS Claimed_Freq, 
-        u.status 
+        p.status AS Policy_Status
     FROM users u
     LEFT JOIN policies p ON u.id = p.user_id
      LEFT JOIN claims c
@@ -22,13 +38,15 @@ exports.userLists = (db) => {
         u.name,
         p.policy_number,
         p.policy_id,
-        u.status
+        p.status
   `;
     db.query(sql, (err, results) => {
       if (err) return res.status(500).json(err);
       res.json(results);
+     });
+
     });
+
   };
 };
-
 //khaing mon mon zaw
