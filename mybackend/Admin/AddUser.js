7874f58c85b7de4a04f9beacdd6b71b5996394dd
eaint thiri phyo <eaintthiriphyo1@gmail.com>
2026-07-21 16:45:51 +0630

    update user add vehicle

--- mybackend/Admin/AddUser.js
+++ mybackend/Admin/AddUser.js
@@ -6,14 +6,14 @@ async function generatePolicyNumber(db) {
       (err, result) => {
         if (err) return reject(err);
 
-        let newNumber = "POL-0001"; 
+        let newNumber = "POL-0001";
         if (result.length > 0 && result[0].policy_number) {
-          const lastPolicy = result[0].policy_number; 
-          const numPart = parseInt(lastPolicy.split("-")[1]); 
+          const lastPolicy = result[0].policy_number;
+          const numPart = parseInt(lastPolicy.split("-")[1]);
           newNumber = `POL-${String(numPart + 1).padStart(4, "0")}`;
         }
         resolve(newNumber);
-      }
+      },
     );
   });
 }
@@ -23,13 +23,12 @@ exports.getNextPolicyNumber = (db) => {
       const policyNumber = await generatePolicyNumber(db);
 
       res.json({
-        policyNumber: policyNumber
+        policyNumber: policyNumber,
       });
-
     } catch (err) {
       console.error(err);
       res.status(500).json({
-        message: "Failed to generate policy number"
+        message: "Failed to generate policy number",
       });
     }
   };
@@ -68,61 +67,48 @@ WHERE email=?
 OR driver_license=?
 `;
 
-db.query(
-  checkUserSql,
-  [email, driverLicense],
-  async (err, userResult) => {
-
-    if (err) {
-      return res.status(500).json(err);
-    }
-
-
-    if (userResult.length > 0) {
-
-      const existingUser = userResult[0];
-
-        if (existingUser.driver_license === driverLicense && existingUser.email === email) {
-              console.log("SAME USER FOUND:", existingUser);
-             userId = existingUser.id;
-       return saveVehicle(userId);
+    db.query(checkUserSql, [email, driverLicense], async (err, userResult) => {
+      if (err) {
+        return res.status(500).json(err);
       }
 
-      if (existingUser.email === email) {
-        return res.status(400).json({
-          message: "Email already exists."
-        });
+      if (userResult.length > 0) {
+        const existingUser = userResult[0];
+
+        if (
+          existingUser.driver_license === driverLicense &&
+          existingUser.email === email
+        ) {
+          console.log("SAME USER FOUND:", existingUser);
+          userId = existingUser.id;
+          return saveVehicle(userId);
+        }
+
+        if (existingUser.email === email) {
+          return res.status(400).json({
+            message: "Email already exists.",
+          });
+        }
+
+        if (existingUser.nrc === nrc) {
+          return res.status(400).json({
+            message: "NRC already exists.",
+          });
+        }
+
+        if (existingUser.driver_license === driverLicense) {
+          return res.status(400).json({
+            message: "Driver License already exists.",
+          });
+        }
       }
 
+      // No duplicate → continue insert user
 
-      if (existingUser.nrc === nrc) {
-        return res.status(400).json({
-          message: "NRC already exists."
-        });
-      }
+      const hashPassword = await bcrypt.hash("password123", 10);
 
-
-      if (existingUser.driver_license === driverLicense) {
-        return res.status(400).json({
-          message: "Driver License already exists."
-        });
-      }
-
-       
-
-    }
-
-
-    // No duplicate → continue insert user
-
-    const hashPassword = await bcrypt.hash(
-      "password123",
-      10
-    );
-
-
-    db.query(
-      `
+      db.query(
+        `
       INSERT INTO users
       (
       name,
@@ -138,33 +124,29 @@ db.query(
       )
       VALUES(?,?,?,?,?,?,?,?,?,?)
       `,
-      [
-        fullName,
-        phone,
-        email,
-        nrc,
-        address,
-        dob,
-        driverLicense,
-        drivingYear,
-        "user",
-        hashPassword
-      ],
-      (err,result)=>{
+        [
+          fullName,
+          phone,
+          email,
+          nrc,
+          address,
+          dob,
+          driverLicense,
+          drivingYear,
+          "user",
+          hashPassword,
+        ],
+        (err, result) => {
+          if (err) {
+            return res.status(500).json(err);
+          }
 
-        if(err){
-          return res.status(500).json(err);
-        }
+          userId = result.insertId;
 
-        userId=result.insertId;
-
-        saveRisk(userId);
-
-      }
-    );
-
-  }
-);
+          saveRisk(userId);
+        },
+      );
+    });
     // 4. Insert Risk
     function saveRisk(userId) {
       db.query(
@@ -195,46 +177,73 @@ WHERE vehicle_number=?
 `,
         [vehicleNumber],
 
-        (err, result) => {
-          if (err) return res.status(500).json(err);
-
-          if (result.length > 0) {
-            return res.status(400).json({
-              message: "Vehicle number already exists.",
-            });
+        (err, vehicleResult) => {
+          if (err) {
+            return res.status(500).json(err);
           }
 
-          const vehicleSql = `
-        INSERT INTO vehicles
-        (
-          vehicle_number,
-          vehicle_model,
-          model_year
-        )
-        VALUES(?,?,?)
-      `;
+          if (vehicleResult.length > 0) {
+            const vehicleId = vehicleResult[0].vehicle_id;
 
-          db.query(
-            vehicleSql,
-            [vehicleNumber, vehicleModel, modelYear],
-            (err, vResult) => {
-              if (err) {
-                return res.status(500).json(err);
-              }
+            // check active policy
+            db.query(
+              `
+SELECT policy_id
+FROM policies
+WHERE vehicle_id=?
+AND status='active'
+`,
+              [vehicleId],
 
-              const vehicleId = vResult.insertId;
+              (err, policyResult) => {
+                if (err) {
+                  return res.status(500).json(err);
+                }
 
-              savePolicy(vehicleId,userId);
-            },
-          );
+                if (policyResult.length > 0) {
+                  return res.status(400).json({
+                    message: "This vehicle already has an active policy.",
+                  });
+                }
+
+                // no active policy
+                // reuse vehicle
+
+                savePolicy(vehicleId, userId);
+              },
+            );
+          } else {
+            // new vehicle
+
+            db.query(
+              `
+INSERT INTO vehicles
+(
+vehicle_number,
+vehicle_model,
+model_year
+)
+VALUES(?,?,?)
+`,
+              [vehicleNumber, vehicleModel, modelYear],
+
+              (err, result) => {
+                if (err) {
+                  return res.status(500).json(err);
+                }
+
+                savePolicy(result.insertId, userId);
+              },
+            );
+          }
         },
       );
     }
 
     // 3. Insert Policy
-    async function savePolicy(vehicleId,userId) {
-          console.log("savePolicy called");
-    const generatedPolicyNumber = await generatePolicyNumber(db);
+    async function savePolicy(vehicleId, userId) {
+      console.log("savePolicy called");
+      const generatedPolicyNumber = await generatePolicyNumber(db);
       /*db.query(
         `
 SELECT policy_id
@@ -252,7 +261,7 @@ WHERE policy_number=?
             });
           }*/
 
-          const policySql = `
+      const policySql = `
         INSERT INTO policies
         (
           policy_number,
@@ -264,37 +273,38 @@ WHERE policy_number=?
           remaining_balance,
           total_coverage,
           monthly_premium,
-          policy_duration
+          policy_duration,
+          status
         )
-        VALUES(?,?,?,?,?,?,?,?,?,?)
+        VALUES(?,?,?,?,?,?,?,?,?,?,?)
       `;
 
-          db.query(
-            policySql,
-            [
-              generatedPolicyNumber,
-              userId,
-              vehicleId,
-              startDate,
-              endDate,
-              totalPremium,         
-              coverageLimit,
-              coverageLimit,
-              monthlyPremium,
-              policyDuration,
-            ],
-            (err, pResult) => {
-              if (err) {
-                console.error("Policy Insert Error:",err);
-                return res.status(500).json(err);
-                 
-              }
+      db.query(
+        policySql,
+        [
+          generatedPolicyNumber,
+          userId,
+          vehicleId,
+          startDate,
+          endDate,
+          totalPremium,
+          coverageLimit,
+          coverageLimit,
+          monthlyPremium,
+          policyDuration,
+          "active",
+        ],
+        (err, pResult) => {
+          if (err) {
+            console.error("Policy Insert Error:", err);
+            return res.status(500).json(err);
+          }
 
-              const policyId = pResult.insertId;
+          const policyId = pResult.insertId;
 
-              saveCoverage(policyId);
-            } 
-          );  
+          saveCoverage(policyId);
+        },
+      );
     }
 
     // 5. Insert Coverage
